c8cf79c926a91128e34c85134dead13f2a793c19
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-03 12:58:09 +0330

    updit Sidebar and map file

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,31 @@ import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound";
- 
+
 function App() {
   return (
-  
-    
-    <BrowserRouter>
-      
 
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/app" element={<AppLayout />} >
-          <Route index element={<p>LIST</p>} />
-          <Route path="cities" element={<p>List of cities</p>}/>
-          <Route path="countries" element={<p>Countries</p>}/>
-          <Route path="form" element={<p>Form</p>}/>
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-     
-    </BrowserRouter> 
- 
+
+    <BrowserRouter>
+
+
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/app" element={<AppLayout />} >
+           <Route index element={<p>LIST</p>} />
+           <Route path="cities" element={<p>List of cities</p>} />
+           <Route path="countries" element={<p>Countries</p>} />
+           <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
