d13485f1270e2cc09e135453638ac02e6a100f75
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-04 16:11:00 +0330

    Update  file ctiylist and app  component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,29 @@ import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound";
-
+import Ctiylist from "./Component/CityList";
+import { useEffect, useState } from "react";
+ const BASE_URL = "http://localhost:9000/cities"
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoding , setIsLoding] = useState(false);
+
+  useEffect(function(){
+    async function fetchCities() {
+      try{
+        setIsLoding(true)
+         const res =  await fetch(`${BASE_URL}/cities`);
+      const data = await res.json();
+      setCities(data); 
+      }
+    catch  {
+      alert("There was an error loading data...")
+    }finally{
+      setIsLoding(false)
+    }
+    }
+ 
+  },[cities])
   return (
 
 
@@ -21,11 +42,11 @@ function App() {
 
         <Route path="/app" element={<AppLayout />} > 
 
-           <Route index element={<p>LIST</p>} />
-           <Route path="cities" element={<p>List of cities</p>} />
+           <Route index element={<Ctiylist/>} />
+           <Route path="cities" element={<Ctiylist/>} />
            <Route path="countries" element={<p>Countries</p>} />           
            <Route path="form" element={<p>Form</p>} />
-           
+
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
