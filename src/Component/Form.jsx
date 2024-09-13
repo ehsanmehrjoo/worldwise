51c385ddc7a703260c10d759e97146d7f288fce0
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-13 19:09:18 +0330

    add BackButton component

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -15,7 +15,7 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate =  useNavigate();
+
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
