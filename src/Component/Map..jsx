8f769bed52be4bb0fc01ec27fc231205ecd4e068
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-14 09:52:44 +0330

    Update Map and add jsx

--- src/Component/Map..jsx
+++ src/Component/Map..jsx
@@ -1,19 +1,26 @@
 import {  useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
  function Map ()  {
  const navigate = useNavigate();
-
+ const position = [51.505, -0.09]
  const [searchParam , setSearchParam ] = useSearchParams();
  const lat = searchParam.get("lat");
  const lng = searchParam.get("lng")
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-       <h1>
-        Map
-       </h1>
-       <h1>Position : {lat} , {lng}</h1>
-      <button onClick={() => setSearchParam({lat : 50 , lng : 40 })}>Change position</button>
+         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
     </div>
   );
 };
