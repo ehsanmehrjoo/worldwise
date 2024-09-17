33c66715d13bc93d55c5bf54b73a46715014bbef
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-17 21:32:46 +0330

    Setting up the website login logic

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,13 +1,21 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext } from 'react'
 
 const AuthContext = createContext()
 
 function  AuthProvider ({children}) {
+    
+    function login(email , padssword){}
+
+    function logout (){}
   return (
     <AuthContext.Provider>
         {children}
     </AuthContext.Provider>
   )
 }
-
-export   {AuthProvider}
+function useAuth (){
+    const context = useContext(AuthContext)
+    if(context === undefined) throw new Error ("AuthContext  was used outside the AuthProvider")
+    return context
+}
+export   {AuthProvider , useAuth}
