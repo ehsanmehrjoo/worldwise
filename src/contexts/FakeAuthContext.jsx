9be150aa68dd2d0cd59b01326e620df873fc3a92
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-17 21:47:50 +0330

    Setting up the website login logic

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,12 +1,34 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useReducer, useState } from 'react'
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
+const initialState = {
+    user : null, 
+      isAuthenticated : false
+}
+function reducer(state , action){
+    switch (action.type) {
+        case  "login":
+            return {...state , user  : action.payload , isAuthenticated : true}
+            
+          
+    case "logout" :
+        return {...state , user : null , isAuthenticated : true}
+        default:
+            throw new Error ("Unknown action type")
+             
+    }
+}
 function  AuthProvider ({children}) {
+    const [{user ,  isAuthenticated} , dispatch] = useReducer(reducer)
     
-    function login(email , padssword){}
+    function login(email , padssword){
+        dispatch({type : "login" , payload : email , padssword})
+    }
 
-    function logout (){}
+    function logout (){
+        dispatch({type : "logout"})
+    }
   return (
     <AuthContext.Provider>
         {children}
