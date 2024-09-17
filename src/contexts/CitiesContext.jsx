5bddafb62988cd3e35b1cfcc3f320219a61f40c5
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-17 11:27:03 +0330

    Update

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,13 +1,23 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-
-const CitiesContext = createContext();
-
+import React, { createContext, useContext, useEffect, useReducer, useState } from 'react';
 const BASE_URL = "http://localhost:8000";
 
+const CitiesContext = createContext();
+ const initialState = {
+  cities : [] ,
+  isLoading : false, 
+  currentCity : {}
+ }
+function reducer(state , action){
+  switch (action.type) {
+    
+  }
+}
+
 function CitiesProvider({children}) {
-    const [cities, setCities] = useState([]); 
-    const [isLoading, setIsLoading] = useState(false);
-    const [currentCity , setCurrentCity] = useState({});
+  const [ {cities  , isLoading  , currentCity}, dispatch] = useReducer(reducer , initialState )
+    // const [cities, setCities] = useState([]); 
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [currentCity , setCurrentCity] = useState({});
 
     
   useEffect(() => {
