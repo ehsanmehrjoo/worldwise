d723dafb8cbb377b1b1514219526a75495644fbd
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-18 19:29:48 +0330

    Update App

--- src/App..jsx
+++ src/App..jsx
@@ -1,5 +1,5 @@
- import { CitiesProvider  } from "./contexts/CitiesContext"
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 
 import Product from "./pages/Product.";
 import Pricing from "./pages/Pricing.";
@@ -13,40 +13,55 @@ import Form from "./Component/Form";
 import CountryList from "./Component/CountryList";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
- 
+import { useEffect } from "react";
 
+function AppContent() {
+  const location = useLocation();
 
-function App() {
+  useEffect(() => {
+    const pageTitle = location.pathname.split("/")[1] || "Homepage";
+    document.title = `WorldWise | ${pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)}`;
 
+    return () => {
+      document.title = "WorldWise // Keep track of your adventures";
+    };
+  }, [location]);
 
   return (
-    <CitiesProvider>
-    <AuthProvider>
+    <Routes>
+      <Route index element={<Homepage />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/login" element={<Login />} />
 
-    
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<Pricing />} />
-        
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/app" element={
+      <Route
+        path="/app"
+        element={
           <ProtectedRoute>
-          <AppLayout />
-        </ProtectedRoute> }>
-          <Route index element={<Navigate to="cities" replace/>} />
-          <Route path="cities" element={<CityList />} />
-          <Route path="cities/:id" element={<City />}/>
-          <Route path="countries" element={<CountryList />} />
-          <Route path="form" element={<Form />} /> 
-        </Route>
+            <AppLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Navigate to="cities" replace />} />
+        <Route path="cities" element={<CityList />} />
+        <Route path="cities/:id" element={<City />} />
+        <Route path="countries" element={<CountryList />} />
+        <Route path="form" element={<Form />} />
+      </Route>
 
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-    </AuthProvider>
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <CitiesProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <AppContent />
+        </BrowserRouter>
+      </AuthProvider>
     </CitiesProvider>
   );
 }
