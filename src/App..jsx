4b6abc203e6d2059157e145a8b4920699544d140
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-27 11:21:29 +0330

    Update app lazy loading component

--- src/App..jsx
+++ src/App..jsx
@@ -1,19 +1,35 @@
-import { CitiesProvider } from "./contexts/CitiesContext";
+import { lazy, useEffect } from "react";
+
 import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 
-import Product from "./pages/Product.";
-import Pricing from "./pages/Pricing.";
-import Homepage from "./pages/Homepage.";
-import AppLayout from "./pages/AppLayout.";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./Component/CityList."; // Corrected name
 import City from "./Component/City"; 
 import Form from "./Component/Form";
 import CountryList from "./Component/CountryList";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
-import { useEffect } from "react";
+
+// import Product from "./pages/Product.";
+// import Pricing from "./pages/Pricing.";
+// import Homepage from "./pages/Homepage.";
+// import AppLayout from "./pages/AppLayout.";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+
+const Homepage = lazy(() => import("./pages/Homepage."))
+const Product = lazy(() => import("./pages/Product."))
+const Pricing = lazy(() => import("./pages/Pricing."))
+const AppLayout = lazy(() => import("./pages/AppLayout."))
+const Login = lazy(() => import("./pages/Login"))
+const PageNotFound = lazy(() => import("./pages/PageNotFound"))
+
+
+
+// dist/index.html                   0.48 kB │ gzip:   0.31 kB
+// dist/assets/index-ff3db4f3.css    9.66 kB │ gzip:   2.48 kB
+// dist/assets/index-dd652cd2.js   336.83 kB │ gzip: 104.88 kB
 
 function AppContent() {
   const location = useLocation();
